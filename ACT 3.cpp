1cb9cf0f4308118e6814df60d08a9a3bae0ab6df
rcedric_246 <rcedric.cruz@gmail.com>
2026-03-31 23:24:22 +0800

    Update ACT 3.cpp

--- ACT 3.cpp
+++ ACT 3.cpp
@@ -6,6 +6,18 @@ int main()
   int year;
   cout <<"Enter a year:";
   cin >> year;
+
+  bool isLeap = false;
+
+  if (year % 400 == 0) {
+    isLeap = true;
+  } else if (year % 100 == 0) {
+    isLeap = false;
+  } else if(year % 4 == 0) {
+    isLeap = true;
+  }
+    
+
   
 
   
