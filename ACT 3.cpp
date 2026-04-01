daeb11d292cbf7cc5eb8a8b58e1fe2a375148c60
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-01 20:53:28 +0800

    Update ACT 3.cpp

--- ACT 3.cpp
+++ ACT 3.cpp
@@ -17,12 +17,5 @@ int main()
         isLeap = true;
     }
 
-    if (isLeap) {
-        cout << year << " is a leap year." << endl;
-    } else {
-        cout << year << " is not a leap year." << endl;
-    }
-
-    return 0;
-}
+  
 
