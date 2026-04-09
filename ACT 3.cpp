f5fd9b3c994b20be2bcd2a538200b187d7646ff3
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-09 17:37:55 +0800

    Update ACT 3.cpp

--- ACT 3.cpp
+++ ACT 3.cpp
@@ -11,9 +11,10 @@ int maain ()
 
   if (year % 400 == 0) {
     isLeap = true; 
-  } else if (year % 4 == 4) {
-    isLeap= false;
-  }
+  } else if (year % 100 == 0) {
+    isLeap = false;
+  } else if (year % 4 == 0)
+    isLeap = true;
 
   
 }
