97171bd4a96912dbe614acdca2c7921981e7ca4e
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-12 18:51:28 +0800

    Implement leap year check in ACT 3.cpp
    Added user input for year and logic to determine if it's a leap year.

--- ACT 3.cpp
+++ ACT 3.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main () {
 
   int year;
+  cout << "Enter a Year: ";
+  cin >> year;
+
+  boot isLeap = false;
+
+  if (year % 400 == 0) {
+    isLeap = true;
+  } else if ( year 
 }
