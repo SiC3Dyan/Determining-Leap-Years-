d3d84872ffb7345fcf9546d92599b56312d8aab6
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-23 23:48:32 +0800

    Update ACT 3.cpp

--- ACT 3.cpp
+++ ACT 3.cpp
@@ -1,17 +1,27 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-int main () {
-
-  int year;
-  cout << "Enter a Year: ";
+int main() 
+{
+    int year;
+    cout << "Enter a year: ";
     cin >> year;
 
-  if (year % 400 ==0) {
-    isLeap = true;
-  } else if (year % 100 ==0) {
-    isLeap = false;
-  } else if (year % 4 ==0) {
-    isLeap = true;
-  }
+    bool isLeap = false;
+
+    if (year % 400 == 0) {
+        isLeap = true;
+    } else if (year % 100 == 0) {
+        isLeap = false;
+    } else if (year % 4 == 0) {
+        isLeap = true;
+    }
+
+    if (isLeap) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+
+    return 0;
 }
